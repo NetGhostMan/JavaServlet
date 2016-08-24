5649cbf9e4c90548aed5fdc95c014620fc111ff2
edison <edisonhu123@sina.com>
2016-08-24 13:34:48 +0800

    Listner

---
+++ src/main/java/com/JavaServlet/Filter/TestHilter.java
@@ -0,0 +1,65 @@
+package com.JavaServlet.Filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class TestHilter
+ */
+@WebFilter("/TestHilter")
+public class TestHilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public TestHilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("filter destroy method");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		System.out.println("filter doFilter method");
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpSession httpSession = httpServletRequest.getSession();
+		if(httpSession.getAttribute("username") == null){
+			HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+			httpServletResponse.sendRedirect("../index.html");
+		}else{
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("filter init method");
+		String value = fConfig.getInitParameter("filterParam");
+		System.out.println(value);
+	}
+
+}
